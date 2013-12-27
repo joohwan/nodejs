6b7382933e1a613d60d79ca58aa8f4d1a35ddf13
joohwan oh <joohwan.oh@gmail.com>
2013-12-26 21:16:36 -0500

    initial commit

---
+++ requestHandlers.js
@@ -0,0 +1,28 @@
+var exec = require("child_process").exec;
+
+function start(response) {
+	console.log("request handler for start called");
+		
+	exec("find /", 
+		{timeout: 10000, maxBuffer: 20000*1024},
+		function(error, stdout, stderr) {
+			response.writeHead(200, {"Content-Type":"text/plain"});
+			response.write(stdout);
+			response.end();
+		});	
+}
+
+function upload(response) {
+	console.log("request handler for upload called");
+	response.writeHead(200, {"Content-Type":"text/plain"});
+	response.write("Hello upload");
+	response.end();
+}
+
+function sleep(milliseconds) {
+	var startTime = new Date().getTime();
+	while (new Date().getTime() < (startTime + milliseconds));
+}
+
+exports.start = start;
+exports.upload = upload;
